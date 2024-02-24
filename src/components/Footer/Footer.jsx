b944ee45b7cd00a925fcd3452fcc6572f188bf62
biswarup-naha <jeeeeet6902@gmail.com>
2024-02-24 12:04:28 +0530

    built at once

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Grid, Link, Typography } from '@mui/material';
+
+import starWarsLogoGitHub from '../../assets/starwars-planet-icon-github.png';
+
+const Footer = function Footer() {
+  return (
+    <Grid item sx={{ marginTop: 5 }}>
+      <footer>
+        <Typography variant="body1" align="center">
+          A simple project with all the <strong>Must-Have features</strong> I&apos;d like to add to
+          any <strong>React</strong> project.
+          <br />
+          <Link href="https://github.com/gabrielizalo/starwars-planets">
+            <img alt="StarWars Planets GitHub" src={starWarsLogoGitHub} />
+          </Link>
+        </Typography>
+      </footer>
+    </Grid>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Grid, useMediaQuery } from '@mui/material';
+
+import starWarsLogoLandscape from '../../assets/Star-wars-logo-new-tall.png';
+import starWarsLogoPortrait from '../../assets/star-wars-portrait.jpg';
+
+const Header = function Header() {
+  const isPortrait = useMediaQuery('(orientation: portrait)');
+
+  return (
+    <Grid item sx={{ marginBottom: 5 }}>
+      <header>
+        <img
+          alt="StarWars Planets"
+          src={isPortrait ? starWarsLogoPortrait : starWarsLogoLandscape}
+          width="100%"
+        />
+      </header>
+    </Grid>
+  );
+};
+
+export default Header;
